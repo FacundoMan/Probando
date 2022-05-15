0c74237002decdb1e7249d389a65efdb0c98be51
FacundoMan <fmansillla@gmail.com>
2022-05-15 16:11:46 -0300

    probando 2

---
+++ src/appdata/Asdad.java
@@ -0,0 +1,12 @@
+package appdata;
+
+public class Asdad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("probando");
+		System.out.println("probando2");
+
+	}
+
+}
